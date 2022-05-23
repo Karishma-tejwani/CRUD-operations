09e75a88a93e2604b69465063a276a4da1006b48
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-05-23 13:01:30 +0500

    apply css

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const http = require('http');
-const fs = require('fs');
-const { render, compileFile } = require('pug');
+// const http = require('http');
+// const fs = require('fs');
+// const { render, compileFile } = require('pug');
 const express = require('express');
 const app = express()
 const path = require('path');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //import db
 
 //connect db
 mongoose.connect("mongodb://localhost:27017/ead");
 
-//import path of products
-const Product = require('./models/Product')
+//import products
+const Product = require('./models/Product');
 
 // apply css 
-app.use('/style', express.static('public/css/'))
+app.use('/style', express.static('public/css/'));
 
+const bodyParser = require('body-parser');
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//set view engine
 app.set('view engine', 'ejs')
 // app.set('view engine', 'pug')
 
@@ -83,7 +88,7 @@ app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
     const products = ['A','B','C','D']
-    res.render('index',{name:"kd", products})
+    res.render('index',{name:"Man!", products})
 });
 
 app.get('/create', function(req, res){
@@ -92,9 +97,37 @@ app.get('/create', function(req, res){
 
 app.get('/products', async function(req, res){
     const products = await Product.find();
-    res.render('Products', {products});
+    res.render('products', {products});
 });
 
+app.get('/product/getProduct/:pid', async function(req, res){
+    const pid = req.params.pid;
+    console.log(pid);
+    const product = await Product.findById(pid);
+    res.render('productDetail', {product})
+});
+
+// Update
+app.post('/products/update/:pid', async function(req, res){
+    const pid = req.params.pid;
+    console.log("updated id: " + pid);
+    console.log("updated title and price: "+req.body.title+ " " + req.body.price);
+    const prod = await Product.updateMany({_id: pid}, {$set: {title: req.body.title, price: req.body.price}});
+    console.log("updated product " + prod);
+    res.redirect('/');
+});
+
+// Delete
+app.post('/products/delete/:pid', async function(req, res){
+    const pid = req.params.pid;
+    console.log("delete id: " + pid);
+    const product = await Product.findByIdAndDelete(pid);
+    const products = await Product.find();
+    // console.log("deleted id: " + product);
+    res.render('products', {products});
+});
+
+// Create
 app.post('/product/create', function(req, res){
     console.log(req.body);
     Product.create(req.body, function(err, product){
@@ -103,14 +136,13 @@ app.post('/product/create', function(req, res){
     });
 });
 
-
 // ----------------pug----------
 
 app.get('/blog', function(req, res){
     const products = ['A','B','D','S']
     // const fun = compileFile('views/heading.pug')
-    // console.log(fun({name:'kd'}))
-    res.render('blog',{name:"ditho", products})
+    // console.log(fun({name:'Karshi'}))
+    res.render('blog',{name:"Karshi", products})
 })
 
 app.all('/about', function(req, res){
