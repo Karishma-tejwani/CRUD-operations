00b2350193f70cd437413ba29e102b3026130795
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-05-23 17:39:58 +0500

    image added

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ProductSchema = new Schema({
     title: String,
-    price: Number
+    price: Number,
+    image: String
 });
 const Product = mongoose.model("Product",ProductSchema);
 module.exports = Product;
